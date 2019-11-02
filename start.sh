4ce84bbe4c502fa0285ebd1acea10001ed525adc
Felix Hüttner <huettner94@gmx.de>
2019-11-02 14:55:33 +0100

    Add support for initial user creation

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 ./manage.py migrate
-gunicorn -b :$PORT prb.wsgi
+if [ -n "${DJANGO_ADMIN_USERNAME}" ] && [ -n "${DJANGO_ADMIN_EMAIL}" ] && [ -n "${DJANGO_ADMIN_PASSWORD}" ]; then
+    ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_ADMIN_USERNAME}', '${DJANGO_ADMIN_EMAIL}', '${DJANGO_ADMIN_PASSWORD}') if not User.objects.filter(username='${DJANGO_ADMIN_USERNAME}').exists() else print('User already exists')"
+fi
+gunicorn -b :${PORT} prb.wsgi
